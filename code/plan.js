2ebb5c324d01a3498dac9ce1bb394fc8457a0d45
erdenetuya77 <erdenetuya.edi@gmail.com>
2025-10-30 10:46:35 +0800

    edi oush

---
+++ code/plan.js
@@ -0,0 +1,25 @@
+class route extends HTMLElement {
+    constructor() {
+        super();
+        //implementation
+    }
+
+    connectedCallback() {
+        //implementation
+    }
+
+    disconnectedCallback() {
+        //implementation
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        //implementation
+    }
+
+    adoptedCallback() {
+        //implementation
+    }
+
+}
+
+window.customElements.define('plan', Plan);
