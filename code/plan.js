f452575db196bdbddc200787e524b6ddca47efa1
erdenetuya77 <erdenetuya.edi@gmail.com>
2025-11-02 22:45:13 +0800

    edi push

--- code/plan.js
+++ code/plan.js
@@ -1,11 +1,30 @@
-class route extends HTMLElement {
+class suggestedPoint  extends HTMLElement {
     constructor() {
         super();
-        //implementation
     }
 
     connectedCallback() {
-        //implementation
+        const unelgee=this.getAttribute("rating") || "0.0";
+        const title = this.getAttribute("title") ||"Unknown";
+        const location = this.getAttribute("area") || "unknown";
+        const liked = this.getAttribute("like") || "false"
+        this.innerHTML=`<article class="">
+            <article class="TP">
+                <img class="tp_bg" src="../files/12Landscape1.jpg" alt="">
+                <div class="tp_ic">
+                    <div>
+                        <span class="star checked"></span>
+                        <span class="star checked"></span>
+                        <span class="star checked"></span>
+                        <span class="star checked"></span>
+                        <span class="star"></span>
+                    </div>
+                    <img class="icon" src="../files/add.svg" alt="">
+                </div>
+                <p>Цэгийн нэр</p>
+                <p>Байршил</p>
+            </article>
+        `;
     }
 
     disconnectedCallback() {
