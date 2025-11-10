810ebc75a2938e8df09761c9f409aa6d22a52e4d
erdenetuya77 <erdenetuya.edi@gmail.com>
2025-11-10 15:28:39 +0800

    push

--- code/plan.js
+++ code/plan.js
@@ -3,11 +3,14 @@ class suggestedPoint  extends HTMLElement {
         super();
     }
 
-    connectedCallback() {
-        // const unelgee=this.getAttribute("rating") || "0.0";
-        // const title = this.getAttribute("title") ||"Unknown";
-        // const location = this.getAttribute("area") || "unknown";
-        // const liked = this.getAttribute("like") || "false"
+    // connectedCallback() {
+    //     // const unelgee=this.getAttribute("rating") || "0.0";
+    //     // const title = this.getAttribute("title") ||"Unknown";
+    //     // const location = this.getAttribute("area") || "unknown";
+    //     // const liked = this.getAttribute("like") || "false"
+    // }
+
+    render() {
         this.innerHTML=`
             <article style="background-image: url(../files/12Landscape1.jpg);" class="TP">
                 <div class="tp_ic">
@@ -28,6 +31,9 @@ class suggestedPoint  extends HTMLElement {
         `;
     }
 
+    connectedCallback() { this.render(); }
+    attributeChangedCallback() { this.render(); }
+
     disconnectedCallback() {
         //implementation
     }
@@ -42,45 +48,45 @@ class suggestedPoint  extends HTMLElement {
 }
 window.customElements.define('suggested-point', suggestedPoint);
 
-class planPoint  extends HTMLElement {
-    constructor() {
-        super();
-    }
+// class planPoint  extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
 
-    connectedCallback() {
-        // const unelgee=this.getAttribute("rating") || "0.0";
-        // const title = this.getAttribute("title") ||"Unknown";
-        // const location = this.getAttribute("area") || "unknown";
-        // const liked = this.getAttribute("like") || "false"
-        this.innerHTML=`<article style="background-image: url(../files/12Landscape1.jpg);" class="TP">
-                    <div class="tp_ic">
-                        <svg>
-                            <use href="../styles/icons.svg#icon-star-filled"></use>
-                            <use href="../styles/icons.svg#icon-star-filled"></use>
-                            <use href="../styles/icons.svg#icon-star-filled"></use>
-                            <use href="../styles/icons.svg#icon-star-filled"></use>
-                            <use href="../styles/icons.svg#icon-star-filled"></use>
-                        </svg>
-                        <svg>
-                            <use href="../styles/icons.svg#icon-add"></use>
-                        </svg>
-                    </div>
-                    <p>Цэгийн нэр</p>
-                    <p>Байршил</p>
-                </article>
-        `;
-    }
+//     connectedCallback() {
+//         // const unelgee=this.getAttribute("rating") || "0.0";
+//         // const title = this.getAttribute("title") ||"Unknown";
+//         // const location = this.getAttribute("area") || "unknown";
+//         // const liked = this.getAttribute("like") || "false"
+//         this.innerHTML=`<article style="background-image: url(../files/12Landscape1.jpg);" class="TP">
+//                     <div class="tp_ic">
+//                         <svg>
+//                             <use href="../styles/icons.svg#icon-star-filled"></use>
+//                             <use href="../styles/icons.svg#icon-star-filled"></use>
+//                             <use href="../styles/icons.svg#icon-star-filled"></use>
+//                             <use href="../styles/icons.svg#icon-star-filled"></use>
+//                             <use href="../styles/icons.svg#icon-star-filled"></use>
+//                         </svg>
+//                         <svg>
+//                             <use href="../styles/icons.svg#icon-add"></use>
+//                         </svg>
+//                     </div>
+//                     <p>Цэгийн нэр</p>
+//                     <p>Байршил</p>
+//                 </article>
+//         `;
+//     }
 
-    disconnectedCallback() {
-        //implementation
-    }
+//     disconnectedCallback() {
+//         //implementation
+//     }
 
-    attributeChangedCallback(name, oldVal, newVal) {
-        //implementation
-    }
+//     attributeChangedCallback(name, oldVal, newVal) {
+//         //implementation
+//     }
 
-    adoptedCallback() {
-        //implementation
-    }
-}
-window.customElements.define('plan-point', planPoint);
+//     adoptedCallback() {
+//         //implementation
+//     }
+// }
+// window.customElements.define('plan-point', planPoint);
