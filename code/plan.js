291a531a06ef9a5a6ee223bc4f0bb8f66312f458
erdenetuya77 <erdenetuya.edi@gmail.com>
2025-11-12 16:53:30 +0800

    major push

--- code/plan.js
+++ code/plan.js
@@ -3,36 +3,47 @@ class suggestedPoint  extends HTMLElement {
         super();
     }
 
-    // connectedCallback() {
-    //     // const unelgee=this.getAttribute("rating") || "0.0";
-    //     // const title = this.getAttribute("title") ||"Unknown";
-    //     // const location = this.getAttribute("area") || "unknown";
-    //     // const liked = this.getAttribute("like") || "false"
-    // }
-
-    render() {
+    connectedCallback() {
+        // const unelgee=this.getAttribute("rating") || "0.0";
+        // const title = this.getAttribute("title") ||"Unknown";
+        // const location = this.getAttribute("area") || "unknown";
+        // const liked = this.getAttribute("like") || "false"
+    
         this.innerHTML=`
-            <article style="background-image: url(../files/12Landscape1.jpg);" class="TP">
-                <div class="tp_ic">
-                    <svg>
-                        <use href="../styles/icons.svg#icon-star-filled"></use>
-                        <use href="../styles/icons.svg#icon-star-filled"></use>
-                        <use href="../styles/icons.svg#icon-star-filled"></use>
-                        <use href="../styles/icons.svg#icon-star-filled"></use>
-                        <use href="../styles/icons.svg#icon-star-filled"></use>
+            <section>
+                <div class="top-route">
+                    <svg role="img" area-label="route">
+                        <use xlink:href="../styles/icons.svg#top-route"></use>
                     </svg>
-                    <svg>
-                        <use href="../styles/icons.svg#icon-add"></use>
-                    </svg>
+                    <button type="button" class="add-btn">Нэмэх</button>
                 </div>
-                <p>Цэгийн нэр</p>
-                <p>Байршил</p>
-            </article>
+
+                <div class="rt-details">
+                    <svg id="drag" role="icon" aria-label="drag">
+                            <use xlink:href="../styles/icons.svg#drag"></use>
+                        </svg>
+                    <div class="text-box">    
+                        <h3>Аяллын цэгийн гарчиг</h3>
+                        <p class="loc_text">Газрын байршил</p>
+                        <p>Энэ тайлбарт эхлэх үнэ, цагийн хуваарь байх болно. Мөн анхаарах зүйлсийг хүн өөрөө нэмэлтээр оруулж өөрчилж болно.</p>
+                    </div>
+                    <img src="../files/11statue1.jpg" alt="">
+                    <!-- <span class="delete">Цуцлах</span> -->
+                    <div class="inst">
+                        <svg>
+                            <use href="../styles/icons.svg#icon-add"></use>
+                        </svg>
+                        <label for="guide">Хөтөч сонгох</label>
+                        <select id="guide" name="guide">
+                            <option value=""></option>
+                        </select>
+                    </div>
+                </div>
+            </section>
         `;
     }
 
-    connectedCallback() { this.render(); }
-    attributeChangedCallback() { this.render(); }
+    attributeChangedCallback() { }
 
     disconnectedCallback() {
         //implementation
