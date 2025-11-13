09129b3607be35f17c240eaf82b0de8874c5142c
erdenetuya77 <erdenetuya.edi@gmail.com>
2025-11-13 12:43:25 +0800

    Edi's push

--- code/drag.js
+++ code/drag.js
@@ -1,4 +1,4 @@
-class suggestedPoint  extends HTMLElement {
+class drag  extends HTMLElement {
     constructor() {
         super();
     }
@@ -9,8 +9,7 @@ class suggestedPoint  extends HTMLElement {
         // const location = this.getAttribute("area") || "unknown";
         // const liked = this.getAttribute("like") || "false"
     
-        this.innerHTML=`
-            <section>
+        this.innerHTML=`<section>
                 <div class="top-route">
                     <svg role="img" area-label="route">
                         <use xlink:href="../styles/icons.svg#top-route"></use>
@@ -43,8 +42,6 @@ class suggestedPoint  extends HTMLElement {
         `;
     }
 
-    attributeChangedCallback() { }
-
     disconnectedCallback() {
         //implementation
     }
@@ -57,47 +54,4 @@ class suggestedPoint  extends HTMLElement {
         //implementation
     }
 }
-window.customElements.define('suggested-point', suggestedPoint);
-
-// class planPoint  extends HTMLElement {
-//     constructor() {
-//         super();
-//     }
-
-//     connectedCallback() {
-//         // const unelgee=this.getAttribute("rating") || "0.0";
-//         // const title = this.getAttribute("title") ||"Unknown";
-//         // const location = this.getAttribute("area") || "unknown";
-//         // const liked = this.getAttribute("like") || "false"
-//         this.innerHTML=`<article style="background-image: url(../files/12Landscape1.jpg);" class="TP">
-//                     <div class="tp_ic">
-//                         <svg>
-//                             <use href="../styles/icons.svg#icon-star-filled"></use>
-//                             <use href="../styles/icons.svg#icon-star-filled"></use>
-//                             <use href="../styles/icons.svg#icon-star-filled"></use>
-//                             <use href="../styles/icons.svg#icon-star-filled"></use>
-//                             <use href="../styles/icons.svg#icon-star-filled"></use>
-//                         </svg>
-//                         <svg>
-//                             <use href="../styles/icons.svg#icon-add"></use>
-//                         </svg>
-//                     </div>
-//                     <p>Цэгийн нэр</p>
-//                     <p>Байршил</p>
-//                 </article>
-//         `;
-//     }
-
-//     disconnectedCallback() {
-//         //implementation
-//     }
-
-//     attributeChangedCallback(name, oldVal, newVal) {
-//         //implementation
-//     }
-
-//     adoptedCallback() {
-//         //implementation
-//     }
-// }
-// window.customElements.define('plan-point', planPoint);
+window.customElements.define('drag', drag);
---
+++ code/suggested-point.js
@@ -0,0 +1,42 @@
+class suggestedPoint  extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const unelgee=this.getAttribute("rating") || "0.0";
+        const title = this.getAttribute("title") ||"Unknown";
+        const location = this.getAttribute("area") || "unknown";
+        const liked = this.getAttribute("like") || "false"
+        this.innerHTML=`<article style="background-image: url(../files/12Landscape1.jpg);" class="TP">
+                    <div class="tp_ic">
+                        <svg>
+                            <use href="../styles/icons.svg#icon-star-filled"></use>
+                            <use href="../styles/icons.svg#icon-star-filled"></use>
+                            <use href="../styles/icons.svg#icon-star-filled"></use>
+                            <use href="../styles/icons.svg#icon-star-filled"></use>
+                            <use href="../styles/icons.svg#icon-star-filled"></use>
+                        </svg>
+                        <svg>
+                            <use href="../styles/icons.svg#icon-add"></use>
+                        </svg>
+                    </div>
+                    <h3>Цэгийн нэр</h3>
+                    <p>Байршил</p>
+            </article>
+        `;
+    }
+
+    disconnectedCallback() {
+        //implementation
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        //implementation
+    }
+
+    adoptedCallback() {
+        //implementation
+    }
+}
+window.customElements.define('suggested-point', suggestedPoint);
