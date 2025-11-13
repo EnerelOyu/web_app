e63ebaa0ceb4b09fd5adcdf24b1fd8152f100a22
enereloyu <heenreel@gmail.com>
2025-11-13 22:18:25 +0800

    header.js header.css

---
+++ code/component/ag-header.js
@@ -0,0 +1,47 @@
+class AgHeader extends HTMLElement {
+  connectedCallback() {
+    this.render();
+    this.markActiveLink();
+  }
+
+  render() {
+    this.innerHTML = `
+      <header>
+        <div class="header-logo">
+          <img src="../files/logo.svg" alt="Logo">
+          <h1>Ayal<span>GO</span></h1>
+          <p>Discover Mongolia <br>your way</p>
+        </div>
+        <div class="header-nav">
+          <a href="home.html">Нүүр Хуудас</a>
+          <a href="aylaliin_tseguud.html">Аяллын Цэгүүд</a>
+          <a href="plan.html">Миний Төлөвлөгөө</a>
+          <input type="text" placeholder="Хайлт хийх..">
+          <button class="btn">
+            <svg>
+              <use href="../styles/icons.svg#icon-search"></use>
+            </svg>
+          </button>
+        </div>
+      </header>
+    `;
+  }
+
+  markActiveLink() {
+    let currentPath = window.location.pathname.split("/").pop();
+    if (!currentPath) {
+      currentPath = "home.html"; 
+    }
+
+    const links = this.querySelectorAll(".header-nav a[href]");
+    links.forEach((link) => {
+      const href = link.getAttribute("href");
+      if (href === currentPath) {
+        link.setAttribute("aria-current", "page");
+        link.classList.add("is-active");
+      }
+    });
+  }
+}
+
+customElements.define("ag-header", AgHeader);
