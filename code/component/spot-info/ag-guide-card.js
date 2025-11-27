f8d15808e7aedeb2191eeaf43f05c6d6c1b561bb
enereloyu <heenreel@gmail.com>
2025-11-27 17:47:58 +0800

    rating

--- code/component/spot-info/ag-guide-card.js
+++ code/component/spot-info/ag-guide-card.js
@@ -9,47 +9,13 @@ class AgGuideCard extends HTMLElement {
         
 
     }
-    createStars(rating){
-    const fullStars = Math.floor(rating);
-    const hasHalf = rating - fullStars >= 0.5;
-    const emptyStars = 5 - fullStars - (hasHalf ? 1 : 0);
 
-    let html="";
-
-    for(let i = 0; i < fullStars; i++){
-      html += `
-          <svg class="star">
-              <use href="../styles/icons.svg#icon-star-filled"></use>
-          </svg>
-      `;
-    }
-
-    if(hasHalf){
-      html += `
-          <svg class="star">
-              <use href="../styles/icons.svg#icon-star-half"></use>
-          </svg>
-      `;
-    }
-
-    for(let i = 0; i < emptyStars; i++){
-      html += `
-          <svg class="star">
-              <use href="../styles/icons.svg#icon-star-unfilled"></use>
-          </svg>
-      `;
-    }
-
-    return html;
-
-  }
   render(){
     const name = this.getAttribute("ner") || "unKnown";
         const experience = this.getAttribute("turshilag") || "0";
         const language = (this.getAttribute("hel") || "").split(",").map( t => t.trim()).filter(Boolean);
         const phone = this.getAttribute("utas") || "unKnown";
         const rating = parseFloat(this.getAttribute("unelgee")) || "0.0";
-        const starsHtml = this.createStars(rating);
 
         this.innerHTML = `<article class="guide-card">
                         <div class="guide-info">
@@ -63,12 +29,7 @@ class AgGuideCard extends HTMLElement {
                                 <li><span class="label">Дугаар:</span><span class="value">${phone}</span></li>
                             </ul>
                         </div>
-                        <div class="rating">
-                            <div class="readOnly-rating-stars">
-                                ${starsHtml}
-                            </div>
-                            <p class="rating-num">${rating.toFixed(1)}</p>
-                        </div>
+                        <ag-rating value="${rating}" color = "var(--accent-1)"></ag-rating>
                     </article>`;
 
   }
