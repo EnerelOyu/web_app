cbc0640ae2e9b6922af162248296b9099924b5fc
enereloyu <heenreel@gmail.com>
2025-12-02 13:19:59 +0800

    home page media, footer zaswar

--- code/component/ag-footer.js
+++ code/component/ag-footer.js
@@ -1,162 +1,253 @@
 class AgFooter extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
-    this.css=`
-    footer{
-    background-color: var(--ftr-bg-color);
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: var(--p-md) var(--p-sm);
-    }
+    this.css = `
+      @import url('../styles/fonts.css');
 
-    .footer-left{
+      footer{
+        background-color: var(--ftr-bg-color);
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding: var(--p-md) var(--p-xl);
+        gap: var(--gap-size-xl);
+      }
+
+      .footer-left{
         display: flex;
         align-items: center;
         gap: var(--gap-size-m);
-    }
+      }
 
-    .footer-logo{
+      .footer-logo{
         display: flex;
         flex-direction: column;
         justify-content:flex-start;
-    }
+      }
 
-    .footer-logo img{
+      .footer-logo img{
         width: var(--logo-size);
         height: var(--logo-size);
-    }
+      }
 
-    .footer-logo p{
+      .footer-logo p{
         color:var(--primary);
         font-size: var(--fs-xs);
-        padding: var(--p-md);
-        font-weight: bold ;
+        padding: var(--p-xs) 0;
+        font-weight: bold;
         font-family: 'Rubik';
-    }
+      }
 
-
-    .footer-left h1{
+      .footer-left h1{
         color: var(--bg-color);
         padding-left: var(--p-sm);
         font-size: var(--fs-2xl);
         font-family: 'Rubik';
-    }
+        margin: 0;
+      }
 
-    .footer-left span{
+      .footer-left span{
         color:var(--primary);
-    }
+      }
 
-    .footer-right{
+      .footer-right{
         display: flex;
-        gap: var(--gap-size-m);
+        flex-direction: row;
+        gap: var(--gap-size-xl);
         margin-right: var(--m-md);
-    }
+      }
 
-    .footer-col{
+      .footer-col{
         display: flex;
         flex-direction: column;
         gap: var(--gap-size-s);
-    }
-    .footer-col p{
+      }
+
+      .footer-col p{
         color: var(--primary-5);
         font-family: 'NunitoSans';
         text-transform: uppercase;
         font-size: var(--fs-s);
-    }
+        margin: 0;
+      }
 
-    .footer-list{
+      .footer-list{
         margin: 0;
         padding: 0;
-    }
+      }
 
-    .footer-list li{
+      .footer-list li{
         text-decoration: none;
         color: var(--primary-5);
         list-style: none;
         font-family: 'NunitoSans';
         padding: var(--p-xs) 0;
         font-size: var(--fs-xs);
-    }
+      }
 
-    .footer-list li:hover, .footer-list li a:hover{
+      .footer-list li:hover,
+      .footer-list li a:hover{
         color: var(--primary);
-    }
+      }
 
-    .footer-list li a{
+      .footer-list li a{
         text-decoration: none;
         color: var(--primary-5);
-    }
+      }
 
-    .footer-social{
+      .footer-social{
         display: flex;
         flex-direction: row;
         gap: var(--p-md);
-        margin: 0;
+        margin: var(--m-sm) 0 0;
         padding: 0;
-    }
 
-    .footer-social svg{
+      }
+
+      .footer-social li{
+        list-style: none;
+      }
+
+
+      .footer-social a{
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .footer-social svg{
         height: var(--svg-s);
         width: var(--svg-s);
+        color: var(--primary-5);
         fill: var(--primary-5);
-    }
+        transition: fill 0.3s color 0.3s;
+      }
 
-    .footer-social svg:hover{
+      .footer-social svg:hover{
         fill: var(--primary);
-    }
-    `
+        color: var(--primary);
+      }
+
+      .footer-bottom{
+        width: 100%;
+        margin-top: var(--m-md);
+        text-align: center;
+        font-size: var(--fs-xs);
+        color: var(--primary-5);
+        font-family: 'NunitoSans';
+        opacity: 0.8;
+      }
+
+      /* ====== MEDIA QUERIES ====== */
+
+      /* Tablet */
+      @media (max-width: 900px) {
+        footer{
+          flex-direction: column;
+          align-items: flex-start;
+          padding: var(--p-lg);
+        }
+
+        .footer-right{
+          margin-right: 0;
+        }
+      }
+
+      /* Mobile */
+      @media (max-width: 600px) {
+        footer{
+          align-items: center;
+          text-align: center;
+        }
+
+        .footer-left{
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .footer-left h1{
+          padding-left: 0;
+          font-size: var(--fs-xl);
+        }
+
+        .footer-right{
+          flex-direction: column;
+          align-items: center;
+          gap: var(--gap-size-l);
+          margin-right: 0;
+        }
+
+        .footer-col{
+          align-items: center;
+        }
+
+        .footer-list li{
+          text-align: center;
+        }
+
+        .footer-social{
+          justify-content: center;
+        }
+      }
+    `;
   }
 
+  connectedCallback() {
+    this.render();
+  }
 
-    connectedCallback() {
-        this.render();
+  render() {
+    this.innerHTML = `
+      <style>${this.css}</style>
+      <footer>
+        <div class="footer-left">
+          <div class="footer-logo">
+            <img src="../files/logo.svg" alt="AyalGO logo">
+            <p>Discover Mongolia<br>your way</p>
+          </div>
+          <h1>Ayal<span>GO</span></h1>
+        </div>
 
-    }
-    render() {
-        this.innerHTML = `
-        <style>${this.css}</style>
-        <footer>
-            <div class="footer-left">
-              <div class="footer-logo">
-                <img src="../files/logo.svg" alt="AyalGo logo">
-                <p>Discover Mongolia <br>your way</p>
-              </div>
-              <h1>Ayal<span>GO</span></h1>
-            </div>
-            <div class="footer-right">
-              <div class="footer-col">
-                <p>Тусламж</p>
-                <ul class="footer-list">
-                  <li><a href="">Аяллын цэг хайх</a></li>
-                  <li><a href="../code/guide_sign_up.html">Хөтөчөөр бүртгүүлэх</a></li>
-                  <li><a href=""></a>Аяллын төлөвлөгөө яаж гаргах вэ</li>
-                </ul>
-              </div>
-              <div class="footer-col">
-                <p>Холбоо барих</p>
-                <ul class="footer-list">
-                  <li>Утас: 1234567890</li>
-                  <li>Email: company@email.com</li>
-                  <ul class="footer-social">
-                    <li>
-                      <svg>
-                        <use href="../styles/icons.svg#icon-facebook"></use>
-                      </svg>
-                    </li>
-                    <li>
-                      <svg>
-                        <use href="../styles/icons.svg#icon-instagram"></use>
-                      </svg>
-                    </li>
-                  </ul>
-                </ul>
-              </div>
-            </div>
-        </footer>
-        `;
-    }
+        <div class="footer-right">
+          <div class="footer-col">
+            <p>Тусламж</p>
+            <ul class="footer-list">
+              <li><a href="../code/spots.html">Аяллын цэг хайх</a></li>
+              <li><a href="../code/guide_sign_up.html">Хөтөчөөр бүртгүүлэх</a></li>
+              <li><a href="#">Аяллын төлөвлөгөө яаж гаргах вэ</a></li>
+            </ul>
+          </div>
 
+          <div class="footer-col">
+            <p>Холбоо барих</p>
+            <ul class="footer-list">
+              <li>Утас: 1234-5678</li>
+              <li>Email: company@email.com</li>
+            </ul>
+            <ul class="footer-social">
+              <li>
+                <a href="#" aria-label="Facebook">
+                  <svg>
+                    <use href="../styles/icons.svg#icon-facebook"></use>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" aria-label="Instagram">
+                  <svg>
+                    <use href="../styles/icons.svg#icon-instagram"></use>
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </footer>
+    `;
+  }
 }
 
 window.customElements.define('ag-footer', AgFooter);
