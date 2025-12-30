9a2fef4d5cc122fc26d88d9d5be3ead1afaba83f
annnnnnda69 <anandaebu5@gmail.com>
2025-12-30 18:34:35 +0800

    ana

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,9 +1,6 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
-
-const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Database файлын зам
